a167abe342b4c93c3b145d440ae084449c054795
yousefalm1 <>
2023-09-06 12:21:54 +0000

    Add an empty list to store tasks

--- run.py
+++ run.py
@@ -1,17 +1,28 @@
-
-
-# Display a welcome message
+# creates an empty list to store the tasks the user inputs
+to_do_list = []
 
 def display_welcome_message():
+    """
+    Function to display a welcome message
+    """
     print("Welcome to your own personal TO-DO List")
 
 def add_task():
+    """
+    Function to add a task to the to-do list
+    """
 
 
 def remove_task():
+    """
+    Function to remove a task from the to-do list
+    """
 
 
 def dispaly_list():
+    """
+    Function to display the to-do list
+    """
 
 
 
