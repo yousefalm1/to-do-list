43fea9139fd1ac05eb4bf5b577fb3c4dcb051808
yousefalm1 <>
2023-09-06 13:07:24 +0000

    Add basic python to remove task

--- run.py
+++ run.py
@@ -22,6 +22,16 @@ def remove_task():
     """
     Function to remove a task from the to-do list
     """
+    # User enters the task they wish to remove and that will be stored in "task"
+    task = input("Enter the task to remove: ")
+    # checks if the  what the user inputed is in "to_do_list" 
+    # If the task is present in the "to_do_list" the code below would run which will remove the task
+    if task in to_do_list:
+        to_do_list_remove(task)
+    # If task is not found in the "to_do_list" it will print this statement out.
+    else:
+        print("Task not found in the to-do list")
+
 
 
 def dispaly_list():
