a581a87578bfa5eedf05236cbca1bd1f813391d6
yousefalm1 <>
2023-09-06 12:14:43 +0000

    Add basic structure

--- run.py
+++ run.py
@@ -1,4 +1,17 @@
 
 
 # Display a welcome message
-print("Welcome to your own personal TO-DO List")
+
+def display_welcome_message():
+    print("Welcome to your own personal TO-DO List")
+
+def add_task():
+
+
+def remove_task():
+
+
+def dispaly_list():
+
+
+
